4e3aee8e0205eb050670ddae24f85faa283ed100
Tlowdev <tylorroth6@gmail.com>
2023-04-12 11:12:44 -0700

    addition prog

--- DataValidation.java
+++ DataValidation.java
@@ -3,9 +3,23 @@ public class DataValidation {
 
    public static void main(String [] args) {
       Scanner input = new Scanner(System.in);
-      int kb = val(input);
-      System.out.print(kb);
+      Random rand = new Random();
+      //int kb = val(input);
+      //System.out.print(kb);
+      boolean cont = true;
+    //control loop
+      while(cont) {
+         question(input, rand);
+         System.out.print("Continue? ");
+         String choose = input.next();
+         if (choose.equalsIgnoreCase("no")) {
+            cont = false;
+         } else {
+            question(input, rand);
+         }
+       }
    }
+   
    
    public static int val(Scanner input) {
       
@@ -20,5 +34,25 @@ public class DataValidation {
       }
    
       return age;
+   }
+   //User answers addition questions, if 3 mistakes close out
+   public static void question(Scanner input, Random rand) {
+      
+      int wrong = 0;
+      do {
+         System.out.println("Answer the questions");
+         int num1 = rand.nextInt(20 -1 + 1) + 1;
+         int num2 = rand.nextInt(20 - 1 + 1) + 1;
+         System.out.print(num1 + " + " + num2 + " = ");
+         int answer = input.nextInt();
+         if (num1 + num2 == answer) {
+            System.out.println("Correct");
+         } else {
+            System.out.println("Incorrect");
+            wrong++;
+         }
+         
+      }while(wrong < 3);
+      System.out.println("Thanks for trying");
    }
 }
